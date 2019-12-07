2069ed3cc60b7ad99206054ad5c51f645c77a3c9
DanielJMaia <djohnsonmaia@gmail.com>
2019-12-07 18:50:23 +0000

    created home page and added init js files

---
+++ static/js/initialization.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $('.sidenav').sidenav();
+    $('.tabs').tabs();
+    $('input#input_text, textarea#textarea2').characterCounter();
+    $('select').formSelect();
+});
+
