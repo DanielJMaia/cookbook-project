3c7168dcb2d3dd45a76a40859c833dd66127ecc0
DanielJMaia <djohnsonmaia@gmail.com>
2019-12-07 20:25:43 +0000

    added recipe submission

--- app.py
+++ app.py
@@ -30,6 +30,12 @@ def add_recipe():
     categories=mongo.db.categories.find(),
     difficulty=mongo.db.difficulty.find())
     
+@app.route('/insert_recipe', methods=["POST"])
+def insert_recipe():
+    recipes = mongo.db.recipes
+    recipes.insert_one(request.form.to_dict())
+    return redirect(url_for('/'))
+    
 # Test Page Route
 
 @app.route('/testing')
