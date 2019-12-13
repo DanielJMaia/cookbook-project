067105a895a44bf4cbcb65ffe991e8f6a8f05610
DanielJMaia <djohnsonmaia@gmail.com>
2019-12-13 17:04:39 +0000

    added sweetart alert for deleting recipes

--- static/js/initialization.js
+++ static/js/initialization.js
@@ -5,33 +5,42 @@ $(document).ready(function() {
     $('select').formSelect();
 });
 
+$(document).on("keydown", ":input:not(textarea)", function(event) {
+    return event.key != "Enter";
+});
+
 document.getElementById("back_icon").addEventListener("click", goBack)
-document.getElementById("delete_button").addEventListener("click", showAlert)
+
+
+$('[name="image_url"]').on('change', function() {
+    $('img.image').prop('src', this.value);
+});
 
 function goBack() {
     window.history.back();
 }
 
-$('[name="url"]').on('change', function() {
-    $('img.image').prop('src', this.value);
-});
+document.getElementById("delete_button").addEventListener("click", showAlert)
 
 function showAlert() {
     swal({
             title: "Are you sure?",
-            text: "This will permanently delete this recipe!",
+            text: "This will permanantly delete this recipe.",
             icon: "warning",
             buttons: true,
             dangerMode: true,
         })
         .then((willDelete) => {
             if (willDelete) {
-                swal("The recipe has been deleted!", {
+                swal("The recipe has been deleted", {
                     icon: "success",
                 });
+                setTimeout(function() {
+                    document.getElementById("delete_form_trigger").submit();
+                }, 3000);
             }
             else {
-                swal("Action Cancelled");
+                swal("The recipe is safe!");
             }
         });
 }
